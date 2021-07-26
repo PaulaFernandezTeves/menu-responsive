1a792485adff96df92336ca969b365773696a738
PaulaFernandezTeves <paulafernandezteves@gmail>
2021-07-25 20:58:01 -0500

    new repo

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='btn btn-primary'>Prueba bootstrap</button>
+    </div>
+  );
+}
+
+export default App;
